a577ebd192401a0949d6ab8911091ca27c2ca9b7
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-07-22 23:04:02 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,8 @@
+set1 = {"a","b","c",1, 2, 3}
+set2={1,2,4,5,6}
+set4 = set1.union(set2)
+set5= set1.difference(set2)
+set6=set1.intersection(set2)
+print("Union:",set4)
+print("Difference: ",set5)
+print("Intersection: ",set6)
